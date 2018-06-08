7e0b76de402e5a0efd420827de417cb3f3d3091b
xuhan <xuhan@droi.com>
2018-06-08 15:02:29 +0800

    确定页面布局

---
+++ src/components/pages/Layout.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <top-page></top-page>
+    <nav-page></nav-page>
+    <main-page></main-page>
+  </div>
+</template>
+<style>
+
+</style>
+<script>
+  import TopPage from 'TopPage';
+  import NavPage from 'NavPage';
+  import MainPage from 'MainPage';
+
+  export default {
+    data() {
+      return {}
+    },
+    components: {
+      'top-page': TopPage,
+      'nav-page': NavPage,
+      'main-page': MainPage
+    }
+  }
+</script>
