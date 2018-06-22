7e78c641226177cf2f987eccb2096b6a43fdd2aa
xuhan <xuhan@droi.com>
2018-06-22 18:50:25 +0800

    2018-06-22 代码更新

--- src/components/SignIn.vue
+++ src/components/SignIn.vue
@@ -14,9 +14,10 @@
          <p>账号密码登录</p>
           <div class="usr-psw-wrap">
             <label for="usr">用户名或邮箱</label>
-            <input id="usr" class="signin-filed">
+            <input id="usr" class="signin-filed" autocomplete="off">
             <label for="password">密码</label>
-            <input id="password" class="signin-filed">
+            <input id="password" class="signin-filed" autocomplete="off">
+            <el-button class="signin-submit" type="primary">登录</el-button>
           </div>
           <div class="signup-note">
             <p>没有账号?<router-link to="/join">立即注册</router-link></p>
@@ -53,16 +54,17 @@
     letter-spacing: 2px;
   }
   .usr-psw-wrap {
-
     width: 260px;
-    margin: 20px auto ;
+    margin: 20px auto 30px;
     height: 300px;
     background-color: white;
-    padding: 20px;
+    padding: 30px 20px 0 20px;
     border: 1px solid #d8dee2;
     border-radius:5px;
   }
   .usr-psw-wrap label {
+    text-align: left;
+    margin-left: 20px;
     display: block;
   }
   .signup-note {
@@ -75,8 +77,19 @@
     font-size: 14px;
   }
   .signin-filed {
+    margin-top: 8px;
+    margin-bottom: 10px;
     padding: 6px 8px;
     height: 20px;
+    width: 209px;
+  }
+  .signin-submit{
+    display: block;
+    padding: 6px 8px;
+    height: 40px !important;
+    width: 229px !important;
+    margin: 0 auto;
+    margin-top: 30px;
   }
 </style>
 <script>
--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -1,13 +1,96 @@
 <template>
   <!--注册页面-->
-    <div>
-
+    <div class="signup-page">
+      <div class="header-wrap">
+        <div class="header">
+          <router-link class="header-logo" to="/">
+            <!--这里暂时采用这张图片logo，之后替换为圆框图-->
+            <img  src="../assets/logo.png">
+          </router-link>
+        </div>
+      </div>
+      <div class="signup-wrap">
+        <div class="signup">
+          <p>注册账号</p>
+          <div class="usr-psw-wrap">
+            <label for="usr">用户名或邮箱</label>
+            <input id="usr" class="signup-filed" autocomplete="off">
+            <label for="password">密码</label>
+            <input id="password" class="signup-filed" autocomplete="off">
+            <el-button class="signup-submit" type="primary">注册</el-button>
+          </div>
+          <div class="signup-note">
+            <p>已有账号?<router-link to="/login">立即登录</router-link></p>
+          </div>
+          <p>单车记，乐在其行</p>
+        </div>
+      </div>
     </div>
 </template>
 <style>
-    body{
-        background-color:#ff0000;
-    }
+  .signup-page {
+    height: 100%;
+    background-color: #f9f9f9;
+  }
+  .header-wrap{
+    width: 100%;
+  }
+  .header {
+    width: 100%;
+    padding: 30px 0 20px 0;
+    text-align: center;
+  }
+  .signup-wrap {
+    height: 800px;
+  }
+  .signup {
+    width: 500px;
+    height: 100%;
+    margin: 0 auto;
+    text-align: center;
+  }
+  .signup p {
+    font-size: 20px;
+    letter-spacing: 2px;
+  }
+  .usr-psw-wrap {
+    width: 260px;
+    margin: 20px auto 30px;
+    height: 300px;
+    background-color: white;
+    padding: 30px 20px 0 20px;
+    border: 1px solid #d8dee2;
+    border-radius:5px;
+  }
+  .usr-psw-wrap label {
+    text-align: left;
+    margin-left: 20px;
+    display: block;
+  }
+  .signup-note {
+    width: 280px;
+    padding: 10px;
+    margin: 10px auto;
+    border: 1px solid #d9dee2;
+  }
+  .signup-note p {
+    font-size: 14px;
+  }
+  .signup-filed {
+    margin-top: 8px;
+    margin-bottom: 10px;
+    padding: 6px 8px;
+    height: 20px;
+    width: 209px;
+  }
+  .signup-submit{
+    display: block;
+    padding: 6px 8px;
+    height: 40px !important;
+    width: 229px !important;
+    margin: 0 auto;
+    margin-top: 30px;
+  }
 </style>
 <script>
 
