4b40185b8986cf89c9df1254a3397af743545042
xuhan <xuhan@droi.com>
2018-07-05 19:04:57 +0800

    代码提交 2018-07-05

--- src/components/AddBlog.vue
+++ src/components/AddBlog.vue
@@ -1,20 +1,36 @@
 <template>
-    <div>
-      <top-page></top-page>
+  <div>
+    <top-page></top-page>
+    <div class="blog-content">
+      <div class="blog-title-edit">
+        <span v-show="showBlogTitleTip">请输入标题</span>
+        <textarea class="blog-title" @click="showBlogTitleTip = false" @blur="showBlogTitleTip = true"></textarea>
+      </div>
+
     </div>
+  </div>
 </template>
 <style>
+  .blog-content {
+    width: 800px;
+    margin: 0 auto;
+  }
+
+  .blog-title {
+    resize: none;
+  }
 </style>
 <script>
-import TopPage from '../components/pages/TopPage';
-    export default{
-        data(){
-            return{
-                msg:'hello vue'
-            }
-        },
-      components :{
-          'top-page':TopPage
+  import TopPage from '../components/pages/TopPage';
+
+  export default {
+    data() {
+      return {
+        showBlogTitleTip: true
       }
+    },
+    components: {
+      'top-page': TopPage
     }
+  }
 </script>
