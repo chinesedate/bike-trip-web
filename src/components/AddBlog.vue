30ab64c7f59416f5540c37d848a6c8203d761f21
xuhan <xuhan@droi.com>
2018-07-12 18:38:28 +0800

    代码提交 2018-07-12

--- src/components/AddBlog.vue
+++ src/components/AddBlog.vue
@@ -1,25 +1,33 @@
 <template>
   <div>
     <top-page></top-page>
-    <div class="blog-content">
+    <div class="blog-content-container">
       <div class="blog-title-edit">
         <span class="blog-title-tip" v-show="blogTitleStatus">请输入标题</span>
         <textarea class="blog-title" v-model="blogTitle" @click="blogTitleStatus = false" @blur="showBlogTitleTip"
                   autocomplete="off"></textarea>
       </div>
-      <div class="blog-content-editor"  contenteditable="true">
-        <p>qqqqqqqqqqqqqqqqqqqqqqqqqqq</p>
+      <div class="blog-content-editor" @mouseover="show($event)" @input="updateContent($event)" contenteditable="true">
+        <p v-for="cont in blogContent">
+          {{cont.con}}
+        </p>
       </div>
-
+      <input class="button" type="button" @click="showContent">
     </div>
   </div>
 </template>
 <style>
-  .blog-content-editor{
+  .button {
+    width: 20px;
+    height: 20px;
+  }
+
+  .blog-content-editor {
     height: 50px;
     width: 100%;
   }
-  .blog-content {
+
+  .blog-content-container {
     width: 800px;
     margin: 0 auto;
   }
@@ -48,20 +56,45 @@
     data() {
       return {
         blogTitleStatus: true,
-        blogTitle:"",
-        blogContent:[{
-          text:"fdsfasdfasdfadsf"
-        }]
+        blogTitle: "",
+        blogContent: [
+          {
+            con: "a"
+          },
+          {
+            con: "b"
+          }]
+
       }
     },
     components: {
-      'top-page': TopPage
+      'top-page': TopPage,
+      'test': {
+        render: function (createElement) {
+          return createElement(
+            'h',   // tag name 标签名称
+            this.$slots.default // 子组件中的阵列
+          )
+        }
+      }
     },
-    methods:{
-      showBlogTitleTip(){
+    methods: {
+      showBlogTitleTip() {
         if (this.blogTitle.toString() === "") {
           this.blogTitleStatus = true;
         }
+      },
+      show(event) {
+        // console.log(event.target.innerHTML)
+        // console.log("x:" + event.offsetX + "  y:" + event.offsetY)
+      },
+      showContent() {
+        for (var a of this.blogContent) {
+          console.log(a.con)
+        }
+      },
+      updateContent(event) {
+        console.log(event.target.innerHTML)
       }
     }
   }
