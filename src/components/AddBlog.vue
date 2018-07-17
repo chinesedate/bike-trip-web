e892c6f6de9eb4ec77dae80540cef45368225728
xuhan <3199088399@qq.com>
2018-07-17 23:04:14 +0800

    代码提交home-2018-07-17

--- src/components/AddBlog.vue
+++ src/components/AddBlog.vue
@@ -7,27 +7,78 @@
         <textarea class="blog-title" v-model="blogTitle" @click="blogTitleStatus = false" @blur="showBlogTitleTip"
                   autocomplete="off"></textarea>
       </div>
-      <div class="blog-content-editor">
-        <quill-editor v-model="content"
-                      :options="editorOption"
-                      >
-        </quill-editor>
-      </div>
-
     </div>
+    <div class="blog-content-editor">
+      <quill-editor v-model="content"
+                    :options="editorOption"
+      >
+      </quill-editor>
+    </div>
+    <base-foot class="foot-container"></base-foot>
   </div>
 </template>
+<script>
+  import TopPage from '../components/pages/TopPage';
+  import BaseFoot from './BaseFoot'
+
+  export default {
+    data() {
+      return {
+        blogTitleStatus: true,
+        blogTitle: "",
+        blogContent: [
+          {
+            con: "a"
+          },
+          {
+            con: "b"
+          }],
+        editorOption: {
+          modules: {
+            toolbar: [
+              ['bold', 'italic'],
+              [{'header': 2}],
+              [{'list': 'ordered'}, {'list': 'bullet'}],
+              ['link', 'image', 'video']
+            ]
+          }
+
+        },
+        content: ""
+
+      }
+    },
+    components: {
+      'top-page': TopPage,
+      'base-foot': BaseFoot
+    },
+    methods: {
+      showBlogTitleTip() {
+        if (this.blogTitle.toString() === "") {
+          this.blogTitleStatus = true;
+        }
+      },
+      show(event) {
+        // console.log(event.target.innerHTML)
+        // console.log("x:" + event.offsetX + "  y:" + event.offsetY)
+      },
+      showContent() {
+        for (let a of this.blogContent) {
+          console.log(a.con)
+        }
+      }
+    }
+  }
+</script>
 <style>
-  .button {
-    width: 20px;
-    height: 20px;
-  }
+  .quill-editor:not(.bubble) .ql-container,
+  .quill-editor:not(.bubble) .ql-container .ql-editor {
 
-  .blog-content-editor {
-    height: 50px;
-    width: 100%;
+    height: 500px;
+    padding-bottom: 10px;
   }
-
+</style>
+<style scoped>
   .blog-content-container {
     width: 800px;
     margin: 0 auto;
@@ -49,62 +100,9 @@
     overflow: auto;
     background: 0 0
   }
-</style>
-<script>
-  import TopPage from '../components/pages/TopPage';
 
-  export default {
-    data() {
-      return {
-        blogTitleStatus: true,
-        blogTitle: "",
-        blogContent: [
-          {
-            con: "a"
-          },
-          {
-            con: "b"
-          }],
-        editorOption:{
-          modules:{
-            toolbar:[
-              ['bold', 'italic'],
-              [{'header':2}],
-              [{'list':'ordered'},{'list':'bullet'}],
-              ['link','image','video']
-            ]
-          }
-
-        }
-
-      }
-    },
-    components: {
-      'top-page': TopPage,
-      'test': {
-        render: function (createElement) {
-          return createElement(
-            'h',   // tag name 标签名称
-            this.$slots.default // 子组件中的阵列
-          )
-        }
-      }
-    },
-    methods: {
-      showBlogTitleTip() {
-        if (this.blogTitle.toString() === "") {
-          this.blogTitleStatus = true;
-        }
-      },
-      show(event) {
-        // console.log(event.target.innerHTML)
-        // console.log("x:" + event.offsetX + "  y:" + event.offsetY)
-      },
-      showContent() {
-        for (let a of this.blogContent) {
-          console.log(a.con)
-        }
-      }
-    }
+  .blog-content-editor{
+    width: 800px;
+    margin: 0 auto;
   }
-</script>
+</style>
---
+++ src/components/BaseFoot.vue
@@ -0,0 +1,20 @@
+<template>
+<div class="base-foot">
+  <p>单车记，乐在其行</p>
+</div>
+</template>
+
+<script>
+    export default {
+        name: "BaseFoot"
+    }
+</script>
+
+<style scoped>
+.base-foot{
+  height: 30px;
+  width: 100%;
+  text-align: center;
+  padding: 10px 0;
+}
+</style>
