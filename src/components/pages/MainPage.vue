9f3876cf57f45954f0f1aecb18519ea1209387b6
xuhan <3199088399@qq.com>
2018-09-17 23:32:54 +0800

    代码提交home-2018-09-17

--- src/components/pages/MainPage.vue
+++ src/components/pages/MainPage.vue
@@ -1,20 +1,41 @@
 <template>
-    <div id="main-page">
-      <router-view></router-view>
+  <div id="main-page">
+    <div v-for="(blog, index) in blogList">
+      <p v-html="blog.title"></p>
+      <p v-html="blog.content"></p>
     </div>
+  </div>
 </template>
 <style>
-  #main-page{
+  #main-page {
     height: 100%;
     width: 100%;
   }
 </style>
 <script>
-    export default{
-        data(){
-            return{
-
+  export default {
+    data() {
+      return {
+        blogList:[]
+      }
+    },
+    mounted:function() {
+      this.queryBlogList()
+    },
+    methods: {
+      queryBlogList: function () {
+        const self = this;
+        self.$ajax({
+          method: 'get',
+          url: '/blog/list'
+        }).then(
+          function (res) {
+            if (res.data.status === 1) {
+              self.blogList = res.data.data;
             }
-        }
+          }
+        ).catch();
+      }
     }
+  }
 </script>
