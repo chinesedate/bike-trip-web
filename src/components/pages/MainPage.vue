b4f931ee9e4bd54d726cee40b4cb3b12553ce739
xuhan <3199088399@qq.com>
2018-09-18 20:49:28 +0800

    代码提交home-2018-09-18

--- src/components/pages/MainPage.vue
+++ src/components/pages/MainPage.vue
@@ -1,15 +1,25 @@
 <template>
   <div id="main-page">
-    <div v-for="(blog, index) in blogList">
-      <p v-html="blog.title"></p>
-      <p v-html="blog.content"></p>
-    </div>
+    <ul class="blog-container" v-for="(blog, index) in blogList">
+      <li>
+        <p class="blog-title" v-html="blog.title"></p>
+        <p v-html="blog.content"></p>
+      </li>
+    </ul>
   </div>
 </template>
 <style>
   #main-page {
     height: 100%;
     width: 100%;
+  }
+  .blog-container{
+    list-style: none;
+    width: 1000px;
+    margin: 0 auto;
+  }
+  .blog-title{
+    font-size: medium;
   }
 </style>
 <script>
